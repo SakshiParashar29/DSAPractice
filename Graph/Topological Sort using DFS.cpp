d6bae03867f448a284edeab3ec5dd798cbd61c87
Sakshi Kumari <sakshiparashar2910@gmail.com>
2025-01-15 14:11:15 +0530

    Create Topological Sort using DFS.cpp

---
+++ Graph/Topological Sort using DFS.cpp
@@ -0,0 +1,29 @@
+ void Dfs(vector<vector<int>>& adj, vector<bool>&visited, int u, stack<int>&s)
+    {
+        visited[u] = true;
+        
+        for(int i = 0; i < adj[u].size(); i++)
+        {
+            if(!visited[adj[u][i]])
+               Dfs(adj, visited, adj[u][i], s);
+        }
+        s.push(u);
+    }
+    vector<int> topologicalSort(vector<vector<int>>& adj) {
+       int n = adj.size();
+       
+       vector<bool> visited(n, 0);
+       stack<int> s;
+       vector<int> res;
+       for(int i = 0; i < n; i++)
+       {
+           if(!visited[i])
+             Dfs(adj, visited, i, s);
+       }
+       while(!s.empty())
+       {
+           res.push_back(s.top());
+           s.pop();
+        }
+       return res;
+    }
