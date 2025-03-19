d3cb88edc329cedff54d4b96849f9a73798728d4
Sakshi Kumari <sakshiparashar2910@gmail.com>
2025-03-19 20:29:27 +0530

    Update Permutations.cpp

--- Backtracking/Permutations.cpp
+++ Backtracking/Permutations.cpp
@@ -29,3 +29,28 @@ public:
         return res;
     }
 };
+
+
+// Approach 2 - using idx and with swapping generating all the permutations
+class Solution {
+public:
+    void solve(int idx, vector<vector<int>>& res, vector<int>& nums)
+    {
+        if(idx >= nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+        for(int i = idx; i < nums.size(); i++)
+        {
+            swap(nums[i], nums[idx]);
+            solve(idx + 1,res, nums);
+            swap(nums[i], nums[idx]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        solve(0, res, nums);
+        return res;
+    }
+};
