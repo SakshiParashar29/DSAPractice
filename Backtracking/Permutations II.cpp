0160257c0bb60112d44024a47fe3106fd61a49ef
Sakshi Kumari <sakshiparashar2910@gmail.com>
2025-03-20 08:58:32 +0530

    Update Permutations II.cpp

--- Backtracking/Permutations II.cpp
+++ Backtracking/Permutations II.cpp
@@ -60,3 +60,39 @@ public:
         return res;
     }
 };
+
+
+// Approach 3
+
+class Solution {
+public:
+   vector<vector<int>> res;
+   int n;
+    void solve(int idx, vector<int>& nums)
+    {
+        if(idx == n)
+        {
+            res.push_back(nums);
+            return;
+        }
+        
+        unordered_set<int> s;
+        for(int i = idx; i < n; i++)
+        {
+            if(s.find(nums[i]) != s.end())
+               continue;
+
+            s.insert(nums[i]);
+            swap(nums[i], nums[idx]);
+
+            solve(idx + 1, nums);
+
+            swap(nums[i], nums[idx]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n = nums.size();
+        solve(0, nums);
+        return res;
+    }
+};
