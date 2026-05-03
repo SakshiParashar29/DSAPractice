c2d845b97e51f4a1076c0680f1303930d3167789
Sakshi Kumari <sakshiparashar2910@gmail.com>
2026-05-03 12:27:46 +0530

    Add solution for Buy and Sell Stock with Cooldown
    Implemented three approaches to solve the Buy and Sell Stock with Cooldown problem: recursion, memoization, and bottom-up dynamic programming.

---
+++ DP on stocks/Buy and sell stock with cooldown.cpp
@@ -0,0 +1,77 @@
+// Recursion
+
+class Solution {
+public:
+    int n;
+    int solve(int idx, int buy, vector<int>& prices){
+        if(idx >= n) return 0;
+
+        int profit = 0;
+
+        if(buy){
+            profit = max(-prices[idx] + solve(idx + 1, 0,  prices), solve(idx + 1, 1, prices));
+        }else {
+            profit = max(prices[idx] + solve(idx + 2, 1, prices), solve(idx + 1, 0, prices));
+        }
+
+        return profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+        return solve(0, 1, prices);
+    }
+};
+
+// Memoization
+
+class Solution {
+public:
+    int n;
+    int dp[5001][2];
+
+    int solve(int idx, int buy, vector<int>& prices){
+        if(idx >= n) return 0;
+
+        if(dp[idx][buy] != -1) return dp[idx][buy];
+
+        int profit = 0;
+
+        if(buy){
+            profit = max(-prices[idx] + solve(idx + 1, 0,  prices), solve(idx + 1, 1, prices));
+        }else {
+            profit = max(prices[idx] + solve(idx + 2, 1, prices), solve(idx + 1, 0, prices));
+        }
+
+        return dp[idx][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 1, prices);
+    }
+};
+
+// Bottom-up
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+
+        for(int idx = n - 1; idx >= 0; idx--){
+            for(int buy = 0; buy < 2; buy++){
+                int profit = 0;
+
+                if(buy){
+                    profit = max(-prices[idx] + dp[idx + 1][0], dp[idx + 1][1]);
+                }else{
+                    profit = max(prices[idx] + dp[idx + 2][1], dp[idx + 1][0]);
+                }
+
+                dp[idx][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+};
