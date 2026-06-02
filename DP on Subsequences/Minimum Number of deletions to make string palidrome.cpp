6d5fcca26990fccdebe562aabfba1dda1025ec58
Sakshi Kumari <sakshiparashar2910@gmail.com>
2026-06-02 23:37:42 +0530

    Create Minimum Number of deletions to make string palidrome.cpp

---
+++ DP on Subsequences/Minimum Number of deletions to make string palidrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int lps(string& word1, string& word2){
+        int n = word1.size(), m = word2.length();
+        
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int minDeletions(string s) {
+        int len = s.length();
+        string str = s;
+        reverse(begin(str), end(str));
+        return len - lps(s, str);
+    }
+};
