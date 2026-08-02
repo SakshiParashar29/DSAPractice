ce4628c25810fa37b8a2c4208118e2f8464bed28
Sakshi Kumari <sakshiparashar2910@gmail.com>
2026-08-02 20:39:57 +0530

    Create subset finding using mask.cpp

---
+++ subset finding using mask.cpp
@@ -0,0 +1,12 @@
+// if a 0 0 0, each zero represents don't take that number and 1 represents take that number
+
+// given nums array and size of array is n
+
+for(int i = 0; i < 2^n; i++){
+  for(int mask = 0; mask < n; mask++){
+     if(mask && (1 << i){
+       sum += nums[i];
+     }
+  }
+  subsetSum.push_back(sum);
+}
